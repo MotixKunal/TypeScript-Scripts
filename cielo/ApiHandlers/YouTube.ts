d7b3f9f14986a1102fbc3826c1fc0daa84069f91
Kunal <70906997+Kunal0004@users.noreply.github.com>
2021-03-29 19:56:36 +0100

    turned video id and video link method into one.

--- cielo/ApiHandlers/YouTube.ts
+++ cielo/ApiHandlers/YouTube.ts
@@ -1,4 +1,5 @@
 import { google } from 'googleapis';
+const getVideoId = require('get-video-id')
 
 export default class YouTube {
     private readonly __ApiKey!: string;
@@ -9,32 +10,20 @@ export default class YouTube {
     };
 
     /**
-     * Search the youtube api with video id
-     * @param {string} videoId
-     * @returns Promise<youtube_v3.Schema$Video>
-     */
-
-    public async searchVideoID(videoId: string) {
-        return await google.youtube("v3").videos.list({
-            key: this.__ApiKey,
-            part: ['snippet', 'id', 'topicDetails', 'statistics', 'status', 'contentDetails'],
-            id: [videoId]
-        }).then(response => response.data.items[0])
-    }
-
-    /**
-     * Search the youtube api with video id
-     * @param {string} videolink
+     * Search the youtube api with video id/link
+     * @param {string} videoLinkorId
      * @returns Promise<youtube_v3.Schema$Video>
     */
 
-    public async searchVideoLink(videoLink: string) {
-        let { id } = require('get-video-id')(videoLink);
+    public searchVideoLink(videoLinkorId: string) {
+        let id = getVideoId(videoLinkorId).id || videoLinkorId;
 
-        return await google.youtube("v3").videos.list({
+        return google.youtube("v3").videos.list({
             key: this.__ApiKey,
-            id: id,
+            id: [id],
             part: ['snippet', 'id', 'topicDetails', 'statistics', 'status', 'contentDetails']
         }).then(response => response.data.items[0])
     }
+
+    
 }
