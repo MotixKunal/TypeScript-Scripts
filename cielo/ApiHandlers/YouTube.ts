036f1c3fe2f980a05c11b035fd78a4b30ea6f8bc
Kunal <70906997+Kunal0004@users.noreply.github.com>
2021-03-30 10:16:08 +0100

    Added: searchChannel

--- cielo/ApiHandlers/YouTube.ts
+++ cielo/ApiHandlers/YouTube.ts
@@ -1,5 +1,6 @@
 import { google } from 'googleapis';
 const getVideoId = require('get-video-id')
+const getChannelId = require('get-youtube-channel-id')
 
 export default class YouTube {
     private readonly __ApiKey!: string;
@@ -10,20 +11,34 @@ export default class YouTube {
     };
 
     /**
-     * Search the youtube api with video id/link
+     * Search the youtube api for a video with video id/link
      * @param {string} videoLinkorId
      * @returns Promise<youtube_v3.Schema$Video>
     */
 
-    public searchVideo(videoLinkorId: string) {
+    public async searchVideo(videoLinkorId: string) {
         let id = getVideoId(videoLinkorId).id || videoLinkorId;
 
-        return google.youtube("v3").videos.list({
+        return await google.youtube("v3").videos.list({
             key: this.__ApiKey,
             id: [id],
             part: ['snippet', 'id', 'topicDetails', 'statistics', 'status', 'contentDetails']
         }).then(response => response.data.items[0])
     }
 
-    
+    /**
+     * Search the youtube api for a channel with channel id/link
+     * @param {string} idorlink 
+     * @returns Promise<youtube_v3.Schema$Channel>
+     */
+
+    public async searchChannel(idorlink: string) {
+        let id = await getChannelId(idorlink).then((response: any) => response.id) || idorlink;
+
+        return google.youtube("v3").channels.list({
+            key: this.__ApiKey,
+            id: id,
+            part: ['contentDetails', 'contentOwnerDetails', 'id', 'snippet', 'statistics', 'status', 'topicDetails']
+        }).then(response => response.data.items[0])
+    }
 }
